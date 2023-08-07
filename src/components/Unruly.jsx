f0134351e3123c066e33af32243d9db0bbb34b43
Alan Barry <alanklinect@gmail.com>
2023-08-07 08:26:57 -0500

    different approach to checking progress

--- src/components/Unruly.jsx
+++ src/components/Unruly.jsx
@@ -4,11 +4,13 @@ import { useState, useEffect } from "react";
 
 export default function Unruly() {
     // const [colors, setColors] = useState(gameBoard)
-    const [colors, setColors] = useState(() => {
+    const [currentBoardArray, setCurrentBoardArray] = useState(() => {
         return Array.from({ length: 8 }, () => Array.from({ length: 8 }, getRandomNumber));
     });
     const [selectedBoardSize, setSelectedBoardSize] = useState(8)
-    const [currentBoardArray, setCurrentBoardArray] = useState([])
+    const [hasMoreThanHalf, setHasMoreThanHalf] = useState(false)
+    const threeInRow = ' border-red-500'
+    const moreThanHalf = ' text-red-500 text-center p-2'
 
     useEffect(() => {
         newGame(selectedBoardSize);
@@ -18,12 +20,10 @@ export default function Unruly() {
         0: 'bg-transparent border-black',
         1: 'bg-black border-black',
         2: 'bg-white border-black',
-        3: 'bg-black border-red-500',
-        4: 'bg-white border-red-500'
     }
 
     function handleColorChange(rowIndex, colIndex) {
-        const newColors = [...colors]
+        const newColors = [...currentBoardArray]
         const currentValue = newColors[rowIndex][colIndex];
         switch (currentValue) {
             case 1:
@@ -32,25 +32,10 @@ export default function Unruly() {
             case 2:
                 newColors[rowIndex][colIndex] = 0
                 break
-            case 3:
-                newColors[rowIndex][colIndex] = 2
-                if (newColors[rowIndex][colIndex - 1] === 3) newColors[rowIndex][colIndex - 1] = 1
-                if (newColors[rowIndex][colIndex + 1] === 3) newColors[rowIndex][colIndex + 1] = 1
-                if (newColors[rowIndex - 1][colIndex] === 3) newColors[rowIndex - 1][colIndex] = 1
-                if (newColors[rowIndex + 1][colIndex] === 3) newColors[rowIndex + 1][colIndex] = 1
-                break
-            case 4:
-                newColors[rowIndex][colIndex] = 0
-                if (newColors[rowIndex][colIndex - 1] === 4) newColors[rowIndex][colIndex - 1] = 2
-                if (newColors[rowIndex][colIndex + 1] === 4) newColors[rowIndex][colIndex + 1] = 2
-                if (newColors[rowIndex - 1][colIndex] === 4) newColors[rowIndex - 1][colIndex] = 2
-                if (newColors[rowIndex + 1][colIndex] === 4) newColors[rowIndex + 1][colIndex] = 2
-                break
             default:
                 newColors[rowIndex][colIndex] = 1
         }
-        setColors(newColors)
-        checkProgress(rowIndex, colIndex)
+        setCurrentBoardArray(newColors)
     }
 
     function getRandomNumber() {
@@ -61,88 +46,106 @@ export default function Unruly() {
         const newBoard = Array.from({ length: selectedBoardSize }, () =>
             Array.from({ length: selectedBoardSize }, getRandomNumber)
         );
-        setColors(newBoard);
-        setCurrentBoardArray(newBoard)
+        setCurrentBoardArray(newBoard);
     }
-
 
     function handleBoardSizeChange(event) {
         const newBoardSize = parseInt(event.target.value, 10);
         setSelectedBoardSize(newBoardSize)
     }
 
-    function checkProgress(rowIndex, colIndex) {
-        const selectedValue = currentBoardArray[rowIndex][colIndex];
 
-        const isMatchingHorizontal = (value) => {
-            return (
-                currentBoardArray[rowIndex][colIndex - 1] === value &&
-                currentBoardArray[rowIndex][colIndex + 1] === value
-            );
-        };
+    function getCellClasses(value, rowIndex, colIndex, currentBoardArray) {
+        // Add the necessary logic to determine the classes for each cell based on the conditions
+        let classes = colorMapping[value];
 
-        const isMatchingVertical = (value) => {
-            return (
-                currentBoardArray[rowIndex - 1] &&
-                currentBoardArray[rowIndex + 1] &&
-                currentBoardArray[rowIndex - 1][colIndex] === value &&
-                currentBoardArray[rowIndex + 1][colIndex] === value
-            );
-        };
+        const counts = countColors(currentBoardArray);
 
-        const updateCellsHorizontal = (value) => {
-            currentBoardArray[rowIndex][colIndex] = value;
-            currentBoardArray[rowIndex][colIndex - 1] = value;
-            currentBoardArray[rowIndex][colIndex + 1] = value;
-        };
-
-        const updateCellsVertical = (value) => {
-            currentBoardArray[rowIndex][colIndex] = value;
-            currentBoardArray[rowIndex - 1][colIndex] = value;
-            currentBoardArray[rowIndex + 1][colIndex] = value;
+        // Check if one color has more than half of the current board length
+        const boardLength = currentBoardArray.length * currentBoardArray[0].length;
+        for (const color in counts) {
+            if (counts[color] > boardLength / 2) {
+                classes += moreThanHalf;
+                setHasMoreThanHalf(true)
+                break;
+            }
         }
 
-        if ((selectedValue === 2 && isMatchingVertical(2) && isMatchingHorizontal(2)) ||
-            (selectedValue === 1 && isMatchingVertical(1) && isMatchingHorizontal(1))) {
-            updateCellsHorizontal(selectedValue === 2 ? 4 : 3);
-            updateCellsVertical(selectedValue === 2 ? 4 : 3);
-        } else if (selectedValue === 2 && isMatchingHorizontal(2)) {
-            updateCellsHorizontal(4);
-        } else if (selectedValue === 1 && isMatchingHorizontal(1)) {
-            updateCellsHorizontal(3);
-        } else if (selectedValue === 1 && isMatchingVertical(1)) {
-            updateCellsVertical(3);
-        } else if (selectedValue === 2 && isMatchingVertical(2)) {
-            updateCellsVertical(4);
+        // Check if there are at least 3 consecutive cells of the same color in a row
+        if (value !== 0) {
+            const row = currentBoardArray[rowIndex];
+            if (hasThreeInRow(row, value)) {
+                classes += threeInRow;
+            }
         }
 
+        return classes;
     }
 
+    const countColors = (board) => {
+        const counts = { 0: 0, 1: 0, 2: 0 };
+        board.forEach((row) => {
+            row.forEach((cell) => {
+                counts[cell]++;
+            });
+        });
+        return counts;
+    };
+
+    const hasThreeInRow = (row, color) => {
+        let count = 0;
+        for (let i = 0; i < row.length; i++) {
+            if (row[i] === color) {
+                count++;
+                if (count >= 3) {
+                    return true;
+                }
+            } else {
+                count = 0;
+            }
+        }
+        return false;
+    };
 
 
     return (
         <div>
             <div className="p-2">
-                {colors.map((row, rowIndex) => (
-                    <div key={rowIndex} className='flex' >
+                {currentBoardArray.map((row, rowIndex) => (
+                    <div key={rowIndex} className="flex">
                         {row.map((value, colIndex) => (
                             <div
                                 key={colIndex}
                                 onClick={() => handleColorChange(rowIndex, colIndex)}
-                                className={`w-12 h-12 ${colorMapping[value]} border-4 m-2`}
-                            ></div>
+                                className={`w-12 h-12 ${getCellClasses(
+                                    value,
+                                    rowIndex,
+                                    colIndex,
+                                    currentBoardArray
+                                )} border-4 m-2`}
+                            >
+                                {hasMoreThanHalf ? (
+                                    <span className="absolute inset-0 flex items-center justify-center">
+                                        <span className="text-red-500 text-lg font-bold">!</span>
+                                    </span>
+                                ) : null}
+                            </div>
                         ))}
                     </div>
                 ))}
             </div>
             <button onClick={newGame}>New Game?</button>
             <label>Board Size:</label>
-            <select id='boardSize' value={selectedBoardSize} onChange={handleBoardSizeChange}>
-                <option value='8'>8x8</option>
-                <option value='10'>10x10</option>
-                <option value='12'>12x12</option>
-                <option value='14'>14x14</option>
+            <select
+                id="boardSize"
+                value={selectedBoardSize}
+                onChange={handleBoardSizeChange}
+            >
+                <option value="8">8x8</option>
+                <option value="10">10x10</option>
+                <option value="12">12x12</option>
+                <option value="14">14x14</option>
             </select>
         </div>
-    )
+    );
 }
